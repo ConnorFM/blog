a9d03d7487a0fe54b3fb4c7d862941d29ad2b6eb
Connor FM <foucauld.gaudin@gmail.com>
2019-05-27 10:10:14 +0200

    quête 14

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+
+            // Créer 50 articles
+            for ($j=1; $j <= 50 ; $j++) {
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(3), '</p><p>') . '</p>';
+
+                $article->setTitle(mb_strtolower($faker->sentence()))
+                        ->setContent($content)
+                        ->setCategory($this->getReference('category_' . rand(0,4)));
+
+                $manager->persist($article);
+
+            }
+
+
+        $manager->flush();
+    }
+
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+
+}
